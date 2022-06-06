e8fd93d015f31033753fdd12c0117504891bf798
Liu Yingming <mshow006@gmail.com>
2022-06-06 11:15:41 +0800

    Update env.js

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,3 +1,3 @@
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'dev' ? 'http://localhost:8080' : 'https://peal.cc'
+    baseUrl: process.env.NODE_ENV === 'dev' ? 'http://localhost:8080' : 'https://player.yingming006.cn'
 }
